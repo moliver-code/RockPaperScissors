38563f5ab0d68325f4d6fe13ad0dd61f298c0d46
Matt Oliver <87980329+moliver-code@users.noreply.github.com>
2022-05-06 07:53:30 +0100

    Add UI and refactor game logic
    Simple web interface added to: determine playerSelection when clicked, live scores displayed, final message displayed.
    
    Game logic refactored from previous command line interface to enable this.

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-/*
-I have tried to separate each component into its own function.
-I am not completely happy with the alorithm that determines whether the player has won or lost.
-I don't think it follows the DRY method.
-Nor does the playRound function particularly.
-*/
-
-let roundCounter = 0;
+let roundCounter = -1;
 let playerScore = 0;
 let computerScore = 0;
+
+playerScoreDiv = document.querySelector('.playerScore')
+computerScoreDiv = document.querySelector('.computerScore')
+
+newDiv = document.createElement('div')
+result = document.querySelector('.result')
+result.appendChild(newDiv)
+const endDiv = document.createElement('div')
+endDiv.setAttribute('style','background-color: #9b9bf84d; color: #363687')
+result.appendChild(endDiv)
+
+function updateScores() {
+    playerScoreDiv.textContent = `Player : ${playerScore}`;
+    computerScoreDiv.textContent = `Computer : ${computerScore}`
+    roundCounter += 1;
+    if (roundCounter >= 5) {endGame()}
+};
+updateScores();
+
+const rock = document.querySelector('#rock')
+rock.addEventListener('click', e => game('rock'))
+
+const paper = document.querySelector('#paper')
+paper.addEventListener('click', e => game('paper'))
+
+const scissors = document.querySelector('#scissors')
+scissors.addEventListener('click', e => game('scissors'))
+
 const choiceArray = ['rock', 'paper', 'scissors']
 
 function computerPlay() {
@@ -15,75 +36,85 @@ function computerPlay() {
     return choiceArray[randomNumber];
 };
 
-function playerPlay() {
-    result = (prompt('Paper, scissors or rock?', '').toLowerCase());
-    if (result === 'paper' || result === 'scissors' || result === 'rock' ) {
-        return result;
+function playerPlay(selection) {
+    newDiv.textContent(`selection is ${selection}`)
+    if (selection === 'paper' || selection === 'scissors' || selection === 'rock' ) {
+        return selection;
     }
-    alert('You mispelled something.');
-    playerPlay1();
+    newDiv.textContent ='You mispelled something.';
 };
 
 function chooseRock(computerSelection, playerSelection) {
     if (computerSelection == 'scissors'){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return true
+        newDiv.textContent =`You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        playerScore++;
+        updateScores();
     }
     if (computerSelection == 'paper'){
-        console.log(`You Lose! ${playerSelection} beats ${computerSelection}`);
-        return false
+        newDiv.textContent =`You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+        updateScores()
     }
 };
 
 function choosePaper(computerSelection, playerSelection) {
     if (computerSelection == 'rock'){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return true
+        newDiv.textContent =`You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+        updateScores()
     }
     if (computerSelection == 'scissors'){
-        console.log(`You Lose! ${playerSelection} beats ${computerSelection}`);
-        return false
+        newDiv.textContent =`You Lose! ${playerSelection} beats ${computerSelection}`;
+        computerScore++;
+        updateScores()
     }
 };
 
 function chooseScissors(computerSelection, playerSelection) {
     if (computerSelection == 'paper'){
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return true
+        newDiv.textContent =`You win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+        updateScores()
     }
     if (computerSelection == 'rock'){
-        console.log(`You Lose! ${playerSelection} beats ${computerSelection}`);
-        return false
+        newDiv.textContent =`You Lose! ${playerSelection} beats ${computerSelection}`;
+        computerScore++;
+        updateScores()
     }
 };
 function gameDraw() {
-    console.log(`'It's a draw!`);
-    playRound();
+    newDiv.textContent =`'It's a draw!`;
 };
 
-function playRound() { //playerSelection, computerSelection
-    // determine winner with switch statement?
-    computerSelection = computerPlay();
-    playerSelection = playerPlay();
+function playRound(computerSelection, playerSelection) { 
     if (computerSelection === playerSelection) {gameDraw()};
-    if (playerSelection === 'rock') {return chooseRock(computerSelection, playerSelection)};
-    if (playerSelection === 'scissors') {return chooseScissors(computerSelection, playerSelection)};
-    if (playerSelection === 'paper') {return choosePaper(computerSelection, playerSelection)};
+    if (playerSelection === 'rock') 
+        {return chooseRock(computerSelection, playerSelection)};
+    if (playerSelection === 'scissors') 
+        {return chooseScissors(computerSelection, playerSelection)};
+    if (playerSelection === 'paper') 
+        {return choosePaper(computerSelection, playerSelection)};
 }
 
 function endGame() {
+    console.clear()
+    
     if (playerScore < computerScore) {
-        console.log(`You lose! You scored ${playerScore}, the computer scored ${computerScore}`);
+        endDiv.textContent = `You lose! You scored ${playerScore}, the computer scored ${computerScore}`;
     } else {
-        console.log(`You win! You scored ${playerScore}, the computer scored ${computerScore}`);
+        endDiv.textContent = `You win! You scored ${playerScore}, the computer scored ${computerScore}`;
     }
+    roundCounter = 0;
+    playerScore = 0;
+    computerScore = 0;
 }
 
-function game() {
-    while (roundCounter <5) {
-        roundCounter += 1;
-        playRound() ? (playerScore += 1) : (computerScore += 1);
-    };
-    endGame();
+function game(playerSelection) {
+    if (roundCounter <5) {
+        computerSelection = computerPlay();
+        playRound(computerSelection, playerSelection) 
+    } else {
+        endGame();
+    }
 }
-game();
